dfc72b18258df33ec296b50654014f010334c450
Dom <dom.laskowski@gmail.com>
2012-12-03 13:00:24 -0500

    Add software double buffering

--- rtx_project/user/src/sine.c
+++ rtx_project/user/src/sine.c
@@ -5,23 +5,41 @@
 #include <math.h>
 
 #define PI 3.14159265358979323846f
-#define TRANSFER_COMPLETE_SIGNAL 0x1
+
+#define GENERATE_SINE_SIGNAL 0x1
+#define SWAP_BUFFERS_SIGNAL 0x2
+
+#define XOR(x, y, type) ((type) (((uint32_t) (x)) ^ ((uint32_t) (y))))
 
 static float sampling_period;
 static uint8_t volume;
 
-static osThreadId change_frequency_id;
+static uint16_t buffer0[SINE_TABLE_SIZE];
+static uint16_t buffer1[SINE_TABLE_SIZE];
 
-static uint16_t sine_table[SINE_TABLE_SIZE];
+// Software double buffering
+typedef uint16_t (*buffer_ptr)[SINE_TABLE_SIZE];
+static buffer_ptr front_buffer = &buffer0;
+static buffer_ptr back_buffer = &buffer1;
+
+static osMutexId buffer_mutex;
+osMutexDef(buffer_mutex);
+
 static unsigned int num_samples;
 
 static float new_freq;
 static volatile bool freq_changed = false;
 
 static void generate_sine_helper(float freq);
-static void change_frequency(const void* arg);
 
-osThreadDef(change_frequency, osPriorityNormal, 1, 0);
+static void generator_thread(const void* arg);
+static void swapper_thread(const void* arg);
+
+osThreadDef(generator_thread, osPriorityNormal, 1, 0);
+osThreadDef(swapper_thread, osPriorityNormal, 1, 0);
+
+static osThreadId generator_id;
+static osThreadId swapper_id;
 
 void init_sine_generator(uint32_t freq, uint8_t vol)
 {
@@ -31,7 +49,10 @@ void init_sine_generator(uint32_t freq, uint8_t vol)
 	EVAL_AUDIO_SetAudioInterface(AUDIO_INTERFACE_I2S);
 	EVAL_AUDIO_Init(OUTPUT_DEVICE_AUTO, volume, freq);
 	
-	change_frequency_id = osThreadCreate(osThread(change_frequency), NULL);
+	buffer_mutex = osMutexCreate(osMutex(buffer_mutex));
+	
+	generator_id = osThreadCreate(osThread(generator_thread), NULL);
+	swapper_id = osThreadCreate(osThread(swapper_thread), NULL);
 }
 
 void generate_sine(float freq)
@@ -41,20 +62,20 @@ void generate_sine(float freq)
 	if (first)
 	{
 		generate_sine_helper(freq);
-		Audio_MAL_Play((uint32_t) &sine_table, num_samples);
+		Audio_MAL_Play((uint32_t) *back_buffer, num_samples);
 		first = false;
 	}
 	
 	else
 	{
 		new_freq = freq;
-		freq_changed = true;
+		osSignalSet(generator_id, GENERATE_SINE_SIGNAL);
 	}
 }
 
 void EVAL_AUDIO_TransferComplete_CallBack(uint32_t buffer, uint32_t size)
 {
-	osSignalSet(change_frequency_id, TRANSFER_COMPLETE_SIGNAL);
+	osSignalSet(swapper_id, SWAP_BUFFERS_SIGNAL);
 }
 
 static void generate_sine_helper(float freq)
@@ -62,25 +83,42 @@ static void generate_sine_helper(float freq)
 	num_samples = (unsigned int) ((1 / freq) / sampling_period);
 	const float period = 2 * PI / num_samples;
 	
+	osMutexWait(buffer_mutex, osWaitForever);
+	
 	for (unsigned int i = 0; i < num_samples; ++i)
-		sine_table[i] = (uint16_t) (SINE_AMPLITUDE * sin(i * period) + SINE_AMPLITUDE);
+		(*back_buffer)[i] = (uint16_t) (SINE_AMPLITUDE * sin(i * period) + SINE_AMPLITUDE);
+	
+	osMutexRelease(buffer_mutex);
 }
 
-static void change_frequency(const void* arg)
+static void generator_thread(const void* arg)
 {
 	while (true)
 	{
-		osSignalWait(TRANSFER_COMPLETE_SIGNAL, osWaitForever);
+		osSignalWait(GENERATE_SINE_SIGNAL, osWaitForever);
+		generate_sine_helper(new_freq);
+		freq_changed = true;
+	}
+}
+
+static void swapper_thread(const void* arg)
+{
+	while (true)
+	{
+		osSignalWait(SWAP_BUFFERS_SIGNAL, osWaitForever);
 		
 		if (freq_changed)
 		{
-			EVAL_AUDIO_PauseResume(AUDIO_PAUSE);
 			Audio_MAL_Stop();
+			osMutexWait(buffer_mutex, osWaitForever);
 			
-			generate_sine_helper(new_freq);
+			// Swap buffers
+			front_buffer = XOR(front_buffer, back_buffer, buffer_ptr);
+			back_buffer = XOR(back_buffer, front_buffer, buffer_ptr);
+			front_buffer = XOR(front_buffer, back_buffer, buffer_ptr);
 			
-			Audio_MAL_Play((uint32_t) &sine_table, num_samples);
-			EVAL_AUDIO_PauseResume(AUDIO_RESUME);
+			Audio_MAL_Play((uint32_t) *front_buffer, num_samples);
+			osMutexRelease(buffer_mutex);
 			
 			freq_changed = false;
 		}
